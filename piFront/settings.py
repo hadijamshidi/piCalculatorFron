85dad1a07035ba59ec4b8a6a4be5ffa2de363dd2
unknown <m.hadi.jamshidi@gmail.com>
2020-03-27 14:58:51 +0430

    use server IP for calculator

--- piFront/settings.py
+++ piFront/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = (os.path.join('static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # configs
-PI_URL = 'http://127.0.0.1:8080'
+PI_URL = 'http://37.152.179.211'
 PHONE = '۰۲۱-۸۸۷۷۵۶۴۳'
 INSTAGRAM = 'teacherJamshidi'
 APARAT = 'apparatID'
