4335c0573003b8303159f4700a69332aa517fbfc
unknown <m.hadi.jamshidi@gmail.com>
2020-03-29 14:58:47 +0430

    collect static

--- piFront/settings.py
+++ piFront/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_ROOT = ''
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = ''
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_DIR, '../static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join('static'),)
 MEDIA_URL = '/media/'
